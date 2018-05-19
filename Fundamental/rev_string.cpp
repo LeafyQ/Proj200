d69f4107401ee8d17b43a0f16adb3b2a420b5781
reese <reese@reesedeMacBook-Pro.local>
2018-05-18 20:38:51 -0500

    New branch

---
+++ Fundamental/rev_string.cpp
@@ -0,0 +1,26 @@
+// [Fundamental]Reverse a string
+// "a b c" => "c b a"
+// the algorithm is INPLACE
+// exactly the exercise as Leetcode #151
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <stringstream>
+
+using std::cout;
+using std::string;
+using std::vector;
+using std::endl;
+
+void reverse(string& s){
+}
+
+
+int main(){
+	//fundamental test
+	string s = "Hello, this could be a great start of practice.";
+	cout<<original<<endl;
+	reverse()
+	return 0;
+}
